27025e74f26e3d9a2f9d0951ae952e487c924e1b
kingdavidmartins <King@Kings-Mac-mini.home>
2016-02-04 00:54:49 -0500

    test countFunction against all use cases then runs

--- lowercaseCount.js
+++ lowercaseCount.js
@@ -26,6 +26,8 @@ var lowercaseCount = function () {
 	var checkerArray = [c1, c2, c3, c4, c5, c6];
 	var testArray = [t1, t2, t3, t4, t5, t6];
 
+	var totalTestTestArray = [];
+
 	var countFunction = function (str) {
 
 		var lowercaseAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -44,41 +46,70 @@ var lowercaseCount = function () {
 					};
 				};
 		};
+		
+		totalTestTestArray.push(lowercaseCounterArray.length);
 
-		console.log(lowercaseCounterArray.length);
 	};
 
-	countFunction(t6);
+	//looping through test array and running countFunctions on each value
+	var totalTestArray = [countFunction(t1), countFunction(t2), countFunction(t3),countFunction(t4), countFunction(t5), countFunction(t6)];
 
-		var countFunction = function (str) {
+	
+	//then adding to total of letterscounted. Going to use it to compare to the
+	//the total checkerArray. if equal then countFunction works against all use cases
+	var totalTestArrayResult = totalTestTestArray.reduce(
+		function (a,b) {
+			return a + b;
+		}
+	);
 
-			var str = prompt("What don't believe me? Try it out and input a string?");
+	//adding all the expected results. 
+	var totalCheckerArrayResult = checkerArray.reduce(
+		function (a,b) {
+			return a + b;
+		}
+	);
 
-			var lowercaseAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-			var lowercaseCounterArray = [];
 
-			for (var a = 0; a < str.length; a++) {
+	console.log(totalTestArrayResult);
+	console.log(totalCheckerArrayResult);
+		//if the expected result equals the actual result then
+		//countFunction must be true. Therefore can run it which ever way I want
+		if (totalCheckerArrayResult === totalTestArrayResult) {
+			
+			var countFunction = function (str) {
 
-				var searchStrLetter = str.slice(a,(a + 1));
+				var str = prompt("What don't believe me? Try it out and input a string?");
 
-					for (var b = 0; b < lowercaseAlphabetArray.length; b++) {
+				var lowercaseAlphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-						if (searchStrLetter === lowercaseAlphabetArray[b]) {
+				var lowercaseCounterArray = [];
 
-							lowercaseCounterArray.push(searchStrLetter);
-						};
+					for (var a = 0; a < str.length; a++) {
+
+						var searchStrLetter = str.slice(a,(a + 1));
+
+							for (var b = 0; b < lowercaseAlphabetArray.length; b++) {
+
+								if (searchStrLetter === lowercaseAlphabetArray[b]) {
+
+									lowercaseCounterArray.push(searchStrLetter);
+								};
+							};
 					};
-			};
 
-			//prints the lowercase letters and the letters that was counted
-			console.log(lowercaseCounterArray);
-			console.log(lowercaseCounterArray.length);
+				//prints the lowercase letters and the letters that was counted
+				alert("these are the letters that are lowercase " + lowercaseCounterArray);
+				alert("I counted about " + lowercaseCounterArray.length + " lowercase letters");
 
 			
-		};
+			};
 
 		countFunction();
+
+		};
+		
 
 
 
