6f55554bf31199188073fb4f0f58f6bb38414d6d
King <kingdavidmartins@users.noreply.github.com>
2018-03-09 23:59:32 -0500

    add exOh.scala

---
+++ scala/ex(X)oh(O).scala
@@ -0,0 +1,2 @@
+def exOh = (str: String) = str.toLowercase.split("x") == str.toLowercase.split("o")
+val me: String = "xxxoooxxxoooxoxoxoxoxoxtyketyoxowlfjwefxoxoxoxoxoxoxoxorgliwiefwnfexoxoxoxoxoxoxoxoxoxo"
